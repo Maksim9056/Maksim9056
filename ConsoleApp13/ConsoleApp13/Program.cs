4870d6cf2be3ece440d418ad12d3af3eb37b8ed1
Maksim <bobreczovm@inbox.ru>
2022-07-07 16:26:34 +0300

    задание 2

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            double x;
+            double y;
+            double z;
+            double sum;
+            double REZ;
+            double X = Convert.ToDouble(Console.ReadLine());
+            double Y = Convert.ToDouble(Console.ReadLine());
+            double Z = Convert.ToDouble(Console.ReadLine());
+
+            sum = X + Y + Z;
+            REZ = sum / sum;
+            Console.WriteLine($"{REZ}");
+        }
+    }
+}
