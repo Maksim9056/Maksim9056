0385a445f61c7cd7a34511c8b6483c236ad2cf8b
Maksim <bobreczovm@inbox.ru>
2022-07-07 16:20:18 +0300

    Задание 1

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // задание 1
+
+
+
+            double i;
+            string a;
+            a = Console.ReadLine();
+            if (int.TryParse(a, out int number))
+            { i = Convert.ToInt32(a);
+
+
+                if (i > 0)
+                {
+                    double s = Math.Sqrt(i);
+                    Console.WriteLine(s);
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка невозможно вычислить корень");
+                }
+
+            }
+            else 
+            { Console.WriteLine("Ошибка введено не число"); }
+
+           
+           
+        }
+    }
+}
