551a0c8d4ce54cfdd2ea42f8a7ff326ec9b57a7e
Maksim <bobreczovm@inbox.ru>
2022-09-06 22:58:50 +0300

    no message

---
+++ Первое задание/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1_Задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для вычисления корня:");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double number))
+            {
+
+                if (number >= -0)
+                {
+
+                    double result = Math.Sqrt(number);
+                    Console.WriteLine($"Квадратный корень числа {number} равен {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: корень отрицательного числа вычислить невозможно");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка: {input} не является числом");
+            }
+        }
+    }
+}
