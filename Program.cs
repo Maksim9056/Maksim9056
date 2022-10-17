c20b2c4ecbcf739c9400dcb2b9fade4fe9f2a7df
Maksim9056 <108364585+Maksim9056@users.noreply.github.com>
2022-10-17 22:58:02 +0300

    Тут
    Здесь1

--- Program.cs
+++ Program.cs
@@ -1,30 +1,71 @@
-﻿namespace тест
+﻿namespace Камень_ножницы_бумага_игра
 {
     internal class Program
     {
-        public enum CheckUserWin
-        {
-            PlayerChoice = 0,
-
-            computerChoice = 1,
-        }
-
-        abstract class Player
-        {
-            public abstract CheckUserWin Act();
-        }
-
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int i;
-            i = rnd.Next(1);
-           Console.WriteLine(i);
+            Random random = new Random();
+            int CheckUserWin = random.Next(3);
+            int computerChoice = CheckUserWin;
+            Console.WriteLine(computerChoice);
+            Console.WriteLine("1.Камень");
+            Console.WriteLine("2.Ножницы");
+            Console.WriteLine("3.Бумага");
+            Console.Write("Ваш выбор? — ");
+            string Using = Console.ReadLine();
+
+            if (int.TryParse(Using, out int Player))
 
 
+                Console.WriteLine($"Прошло проверку на число {Using} !");
+
+            else
+
+                Console.WriteLine($"Ошибка {Using} не является числом !");
             
+            
+            Console.WriteLine($" Компьютер:{computerChoice}");
+            Console.Write($"Игрок:{Player}");
+            Console.WriteLine();
+           
+            switch (Player)
+            {
+                case 1:
+                    Console.WriteLine($"Камень против ", computerChoice);
 
+                    if (Player == Player && computerChoice == computerChoice)
+                        Console.WriteLine("Ты победил");
+                    else if (Player == Player && computerChoice == computerChoice)
+                    Console.WriteLine("Ты проиграл");
+                    else
+                        Console.WriteLine("Ничья");
+               
+                    break;
+                case 2:
+                    Console.WriteLine($"Ножницы против ", computerChoice);
 
-        }
+                    if (Player != Player && computerChoice == computerChoice)
+                        Console.WriteLine("Ты победил");
+                    else if (Player != Player && computerChoice == computerChoice)
+                        Console.WriteLine("Ты проиграл");
+                    else
+                        Console.WriteLine("Ничья");
+                 
+
+                    break;
+                case 3:
+                    Console.WriteLine($" Бумага против", computerChoice);
+
+                    if (Player != Player && computerChoice == computerChoice)
+                        Console.WriteLine("Ты победил");
+                    else if (Player != Player && computerChoice == computerChoice)
+                        Console.WriteLine("Ты проиграл");
+                    else
+                        Console.WriteLine("Ничья");
+                     
+
+                    break;
+            }
     }
 }
+    }
