52792101b42929a7e2484a5a6a5de95610a76057
Maksim <bobreczovm@inbox.ru>
2022-10-17 22:52:53 +0300

    no message

---
+++ Камень ножницы бумага/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace тест
+{
+    internal class Program
+    {
+        public enum CheckUserWin
+        {
+            PlayerChoice = 0,
+
+            computerChoice = 1,
+        }
+
+        abstract class Player
+        {
+            public abstract CheckUserWin Act();
+        }
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int i;
+            i = rnd.Next(1);
+           Console.WriteLine(i);
+
+
+            
+
+
+        }
+    }
+}
