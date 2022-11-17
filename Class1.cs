9df5ad9de61c1bc6d2fd15b0067ce67b9dbfa441
Maksim9056 <108364585+Maksim9056@users.noreply.github.com>
2022-11-17 08:50:08 +0300

    XML

--- Class1.cs
+++ Class1.cs
@@ -1,144 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
-namespace Камень_ножницы_бумага4._1
+namespace xml_
 {
     internal class Class1
     {
-        public enum Hand
+        public string Name { get; set; } = "Undefined";
+        public int Age { get; set; }
+        public string Пол { get; set; }
+
+
+        public Class1() { }
+        public Class1(string name, int age, string пол)
         {
-
-            Rock = 1, Paper, Scissorc
-
+            Name = name;
+            Age = age;
+            Пол = пол;
         }
-        public enum Outcome
+        public bool validateRespomse(char responsE)
         {
-            Win, Lose, Tie
+            if (Char.ToUpper(responsE) != 'Y' && Char.ToUpper(responsE) != 'N')
+
+                return true;
+            return false;
         }
-        class УПРАВЛЕНИЕ
+
+        public void Start()
         {
-            public Hand СomputerChoice { get; set; }
-            public Hand PlayerChoice { get; set; }
-            public char UserSelector { get; set; }
+            Class1 person = new Class1();
+            Console.WriteLine("Ввидите имя");
+            person.Name = Console.ReadLine();
+            Console.WriteLine("Ввидите возраст");
+            person.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("M ИЛИ Ж");
+            person.Пол = Console.ReadLine();
+            if (person.Пол == "М" || person.Пол == "Ж")
+                Console.WriteLine($"Пол:{person.Пол}");
+            else
+                Console.WriteLine($"{person.Пол} не выбран повторите снова");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Class1));
 
-
-            public Hand getUserHand()
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("person.xml", FileMode.OpenOrCreate))
             {
-                while (!validateSelection())
-                    Console.Clear();
-                Console.WriteLine("Invalid Inpit");
-                Screen();
-                UserSelector = Convert.ToChar(Console.ReadLine());
-                {
 
-                    switch (Char.ToUpper(UserSelector))
-                    {
-                        case 'R':
-                            PlayerChoice = Hand.Rock;
-                            break;
-                        case 'P':
-                            PlayerChoice = Hand.Paper;
-                            break;
-                        case 'S':
-                            PlayerChoice = Hand.Scissorc;
-                            break;
-                        default:
-                            throw new Exception("Unexpected");
-                    }
-                    return PlayerChoice;
-                }
-
-                  void PlayGame()
-                {
-                    bool gameOver = false;
-                    var rand = new Random();
-                    char responsE;
-                    while (!gameOver)
-                    {
-                        Screen();
-
-                        UserSelector = Convert.ToChar(Console.ReadLine());
-                        getUserHand();
-                        СomputerChoice = (Hand)rand.Next(1, 4);
-                        Console.Clear();
-                        Console.WriteLine(" Computer's Hand{0}", СomputerChoice);
-                        Console.WriteLine("Player's Hand{0}", PlayerChoice);
-                        if (DetermineWiner() == Outcome.Win)
-                            Console.WriteLine("{0} beatse {1}. Player wins", PlayerChoice, СomputerChoice);
-                        else if (DetermineWiner() == Outcome.Lose)
-
-                            Console.WriteLine("{0} beatse {1}. Player wins", СomputerChoice, PlayerChoice);
-
-                        else Console.WriteLine("it's a tie");
-                        Console.WriteLine("\nWould you like to play another game (y or n)");
-                        responsE = Convert.ToChar(Console.ReadLine());
-                        while (validateRespomse(responsE) == false)
-                        {
-                            Console.WriteLine("Invalid input.Please re - enter you selection:");
-                            responsE = Convert.ToChar(Console.ReadLine());
-                        }
-                    }
-
-                    if (responsE == 'N' || responsE =='n')
-                        gameOver = true;
-                    Console.Clear();
-
-                }
-                   bool validateRespomse(char response)
-                {
-                    if (Char.ToUpper(response) != 'Y' && Char.ToUpper(response) != 'N')
-                        return false;
-                    return true;
-                }
-                 Outcome DetermineWiner()
-                {
-
-                    if (PlayerChoice == Hand.Scissorc && СomputerChoice == Hand.Paper)
-                        return Outcome.Win;
-                    else if (PlayerChoice == Hand.Rock && СomputerChoice == Hand.Scissorc)
-                        return Outcome.Win;
-                    else if (PlayerChoice == Hand.Paper && СomputerChoice == Hand.Rock)
-                        return Outcome.Win;
-                    else if (PlayerChoice == Hand.Scissorc && СomputerChoice == Hand.Rock)
-                        return Outcome.Lose;
-                    else if (PlayerChoice == Hand.Rock && СomputerChoice == Hand.Paper)
-                        return Outcome.Lose;
-                    else if (PlayerChoice == Hand.Paper && СomputerChoice == Hand.Scissorc)
-                        return Outcome.Lose;
-                    return Outcome.Tie;
-
-                }
-                  bool validateSelection()
-                {
-                    char value = Char.ToUpper(UserSelector);
-                    if (value != 'R' && value != 'P' && value != 'S')
-                        
-                 return false;
-                    return true;
-
-                }
-
-              //bool validateSelection()
-              //  {
-              //     char value = Char.ToUpper(UserSelector);
-              //      if (value != 'R' && value != 'P' && value != 'S')
-              //          return false;
-              //      return true;
-              //  }
-
-                 void Screen()
-                {
-                    Console.WriteLine("r=Rock");
-                    Console.WriteLine("P-Paper");
-                    Console.WriteLine("S-Scissors");
-                    Console.WriteLine("Please make your selection:");
-                }
+                xmlSerializer.Serialize(fs, person);
+                Console.WriteLine("Объект церилизирован");
 
             }
+            FileInfo.ReferenceEquals(xmlSerializer, person);
+
+            //Console.WriteLine(FileInfo.ReferenceEquals);
+
+
+
         }
-    } 
+
+    }
+    
 }
--- Program.cs
+++ Program.cs
@@ -1,26 +1,69 @@
-﻿namespace Камень_ножницы_бумага4._1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var player = new Class1();
-            char responsee;
-            Console.WriteLine("Would uou to play game of rock,scisorse (y or n)?");
-            responsee = Convert.ToChar(Console.ReadLine());  
+﻿using System.Xml.Serialization;
+using xml_;
 
-            while(player.validateSelection(responsee)== false)
-                    {
-                Console.WriteLine("Invalid Input . Please re-enter your selection ");
-                responsee=Convert.ToChar(Console.ReadLine());
-                if (responsee == 'Y' ||| responsee == 'y')
+namespace xml_
+{
+    using System;
+    using System.Numerics;
+    using System.Runtime.Serialization;
+    using System.Xml.Serialization;
+
+    //public class Person
+    //{
+    //    //        public string Name { get; set; } = "Undefined";
+        //        public int Age { get; set; }
+        //        public string Пол { get; set;}
+
+
+        //        public Person() { }
+        //        public Person(string name, int age,string пол)
+        //        {
+        //            Name = name;
+        //            Age = age;
+        //            Пол= пол;
+        //        }   public bool validateRespomse(char responsE)
+        //        { 
+        //     if (Char.ToUpper(responsE) != 'Y' && Char.ToUpper(responsE) != 'N')
+
+        //                return true;
+        //                return false;
+
+        //}
+        //    }
+        //    }
+
+
+
+
+        internal class Program
+        {
+            static void Main(string[] args)
+
+            {
+
+                Class1 person = new Class1();
+
+
+                char responseE;
+
+                Console.WriteLine("Сыграли бы вы в игру «камень-ножницы» (y или n)?");
+                responseE = Convert.ToChar(Console.ReadLine());
+
+                if (responseE == 'Y' || responseE == 'y')
+
                 {
-                    Console.Clear();
-                 
-                    Console.WriteLine("God bay");
-                    Console.ReadLine();
+
+                    //person.Start();
+                   
+
                 }
+
+
+
+
+
+
             }
         }
     }
-}
+
